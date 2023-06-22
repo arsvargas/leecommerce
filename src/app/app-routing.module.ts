ee2492efded31a02aaf802c8e7191c0e54e95f05
arsvargas <amanda.jaques.vargas@outlook.com>
2023-06-22 18:44:37 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { QuemSomosComponent } from './pages/quem-somos/quem-somos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'contato', component: ContatoComponent},
+  { path: 'quem-somos', component: QuemSomosComponent},
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
+import { ParceirosComponent } from './components/parceiros/parceiros.component';
+import { QuemSomosComponent } from './pages/quem-somos/quem-somos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContatoComponent,
+    MenuPrincipalComponent,
+    ParceirosComponent,
+    QuemSomosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
